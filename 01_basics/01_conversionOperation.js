dcc2a22afbef9aca4e7d22a78f4e7edda9fc3bdf
Mithilesh-Kr-Chaudhary <krishnanmith4@gmail.com>
2025-12-29 20:44:37 +0530

    initial commit

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "33adj";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+let isLoggedIn = 1;
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+// "33"=> 33
+// "33adj" => NaN
+// true => 1; false => 0;
+//
