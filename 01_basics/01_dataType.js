48c522c980a0b62b13dfcba45029d2c213eb4df8
Mithilesh-Kr-Chaudhary <krishnanmith4@gmail.com>
2025-12-29 19:45:37 +0530

    initial commit

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3)  //we  are  using nodejs, not browser
+
+console.log(3 
+  + 3)
+  //code readability should be high
+
+//  MDN ,TC39 or ECMAScript for standard code
+let name = "Mith";
+let roll = 1;
+let isLogged = false;
+let state = null // null => standalone value
+ 
+//number
+//bigInt
+//string
+//boolean
+//undefined 
+//null => standalone(type = object)
+//symbol => unique
+
+//object
+console.log(typeof null);
